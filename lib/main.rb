83adecb896e2df55fd13a08c7cb2fcf8edaf571b
Wei Zhu <wxz138@case.edu>
2016-10-24 22:36:16 -0400

    use cookies to login

--- lib/main.rb
+++ lib/main.rb
@@ -9,7 +9,7 @@ HEADERS_HASH = {"User-Agent" => "Chrome/53.0.2785.143"}
 agent = Mechanize.new
 agent.user_agent = 'Chrome/53.0.2785.143'
 agent.max_history = 1
-agent.cookie_jar.load_cookiestxt("/Users/zhuwei/studying/open/zhSieve/cookies.txt")
+agent.cookie_jar.load_cookiestxt("../cookies.txt")
 
 index_page = agent.get(BASE_URL)
 
