98bda1ad4c28da9dd19a727087b6ecdd9dde7adb
Wei Zhu <wxz138@case.edu>
2016-11-07 21:02:55 -0500

    minor changes

--- lib/zhSieve.rb
+++ lib/zhSieve.rb
@@ -36,23 +36,5 @@ module ZhSieve
 		search_page = agent.get("#{search_uri}")
 		haha = HTMLPage.new(contents:search_page,article_id:article_id).articleMarkdown
 	end
-	# BASE_URL = "https://www.zhihu.com"
-	# question_id = "43830406"
-	# question_uri = "/question/#{question_id}"
-	# answer_id = "46959669"
-	# answer_uri = "#answer-#{answer_id}"
-	# PEOPLE_id = "/people/"
-	# LIST_URL = "#{BASE_URL}/question/51727516#answer-46414810"
-	# HEADERS_HASH = {"User-Agent" => "Chrome/53.0.2785.143"}
 
-	# arguments = ARGV
-
-	# agent = Mechanize.new
-	# agent.user_agent = 'Chrome/53.0.2785.143'
-	# agent.max_history = 1
-	# Dir.chdir(File.dirname(__FILE__))
-	# agent.cookie_jar.load_cookiestxt("../cookies.txt")
-	# # search_page = agent.get(BASE_URL+PEOPLE_URL)
-	# search_page = agent.get("https://www.zhihu.com#{question_uri}#{answer_uri}")
-	# haha = HTMLPage.new(contents:search_page,question_id:question_id,answer_id:answer_id)
 end
